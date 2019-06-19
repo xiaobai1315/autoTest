e0ba8e77b8984f1d5340ae141fe071ec312c8cc5
xiaobai1315 <xiaobai1315@yeah.net>
2019-06-19 09:14:19 +0800

    commit

--- src/main/java/com/automator/TestNG3.java
+++ src/main/java/com/automator/TestNG3.java
@@ -10,33 +10,16 @@ import org.testng.annotations.Test;
 
 public class TestNG3 {
 
-    @DataProvider(name = "data")
-    public Object[][] data() {
-
-        return new Object[][]{
-                {"xiaobai", 20},
-                {"xiaohong", 21},
-                {"xiaohei", 22}
-        };
-    }
-
-    @Test(dataProvider = "data")
-    public void test1(String name, int age) {
-
-        System.out.println("name = " + name + " ,age = " + age);
+    @Test
+    public void test2() {
+//        WebDriver driver = new ChromeDriver();
+//        driver.get("http://www.baidu.com");
+//        Search.search(driver, "selenium");
+        System.out.println("test2");
     }
 
     @Test
-    public void test2() throws InterruptedException {
-        WebDriver driver = new ChromeDriver();
-        driver.get("http://www.baidu.com");
-        Search.search(driver, "selenium");
-        Assert.assertEquals("", "");
-    }
-
-    @Test
-    public void test3() throws InterruptedException {
-        Thread.sleep(5000);
-        System.out.println("test3 + " + Thread.currentThread().getId());
+    public void test3() {
+        System.out.println("test3");
     }
 }
